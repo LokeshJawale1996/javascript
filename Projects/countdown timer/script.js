db1f1d8712fc81535d36f372b7f1a57cc9531231
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-01 13:45:00 +0530

    in process

--- Projects/countdown timer/script.js
+++ Projects/countdown timer/script.js
@@ -1,15 +1,30 @@
 const newYears = "1 Jan 2023";
+const daysE1 = document.getElementById("days");
+const hoursE1 = document.getElementById("hours");
+const minsE1 = document.getElementById("mins");
+const secondsE1 = document.getElementById("seconds");
 
 function countdown() {
   const newYearsDate = new Date(newYears);
   const currentDate = new Date();
 
-  const seconds = (newYearsDate - currentDate) * 1000;
-  const days = Math.floor(seconds / 3600 / 24);
-  console.log(newYearsDate - currentDate);
-  console.log(days);
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds % 60);
+
+  // console.log(days, hours, mins, seconds);
+  daysE1.innerHTML = days;
+  hoursE1.innerHTML = hours;
+  minsE1.innerHTML = mins;
+  secondsE1.innerHTML = seconds;
 }
 
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
 //initial call
-setInterval(countdown, 1000);
 countdown();
+
+setInterval(countdown, 1000);
