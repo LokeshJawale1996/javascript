52637969dbe6e13b9515615e31e1732646bd3471
LokeshJawale1996 <jawalelokesh2411@gmail.com>
2022-06-02 11:41:44 +0530

    quiz app question answer loading part done

--- Projects/quiz-app/script.js
+++ Projects/quiz-app/script.js
@@ -40,3 +40,33 @@ const quizData = [
     correct: "c",
   },
 ];
+
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+let currentQuiz = 0;
+loadQuiz();
+
+function loadQuiz() {
+  const currentQuizData = quizData[currentQuiz];
+  questionEl.innerHTML = currentQuizData.question;
+  a_text.innerHTML = currentQuizData.a;
+  b_text.innerHTML = currentQuizData.b;
+  c_text.innerHTML = currentQuizData.c;
+  d_text.innerHTML = currentQuizData.d;
+  // currentQuestion++;
+}
+
+submitBtn.addEventListener("click", () => {
+  currentQuiz++;
+  if (currentQuiz < quizData.length) {
+    loadQuiz();
+  } else {
+    //TODO:Show results
+    alert("You Quiz finished! ");
+  }
+  loadQuiz();
+});
